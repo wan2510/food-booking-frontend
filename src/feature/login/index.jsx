8bb936d29c6ef82000b43ee78d7f29513ba16030
wan2510 <quannahe186968@fpt.edu.vn>
2025-02-16 21:23:21 +0700

    Hoàn thiện trang login

--- src/feature/login/index.jsx
+++ src/feature/login/index.jsx
@@ -1,34 +1,27 @@
-import "./login.css";
-import { FaEnvelope, FaLock } from "react-icons/fa";
+import { Button, Col, Flex, Form, Input, Row } from 'antd';
+import './login.css';
+import { FaEnvelope, FaLock } from 'react-icons/fa';
+import { LoginForm } from './LoginForm/LoginForm';
+import { Link } from 'react-router-dom';
+import react from '../../assets/icon/react.svg';
 
 const Login = () => {
     return (
         <div className="login-container">
-            <div className="login-box">
-                <div className="login-left">
-                    <img src="your-image-url.jpg" alt="Login Image" className="login-img" />
-                </div>
-                <div className="login-right">
-                    <h2>Member Login</h2>
-                    <form>
-                        <div className="input-group">
-                            <FaEnvelope className="input-icon" />
-                            <input type="email" id="email" name="email" placeholder="Email" />
-                        </div>
-                        <div className="input-group">
-                            <FaLock className="input-icon" />
-                            <input type="password" id="password" name="password" placeholder="Password" />
-                        </div>
-                        <button type="submit">LOGIN</button>
-                    </form>
-                    <div className="extra-links">
-                        Forgot <a href="#">Username?</a> / <a href="#">Password?</a>
-                    </div>
-                    <div className="extra-links">
-                        <a href="#">Create your Account →</a>
-                    </div>
-                </div>
-            </div>
+            <Row className="login-box">
+                <Col span={15}>
+                    <img src={react} alt="Login Image" className="login-img" />
+                </Col>
+                <Col span={9}>
+                    <h2 className='login-form-title'>Đăng nhập</h2>
+                    <LoginForm />
+                    <Flex className='extra-links' justify='center' gap={20}>
+                        <Link to={'#'}>Quên mật khẩu</Link>
+                        {'/'}
+                        <Link to={'#'}>Đăng ký</Link>
+                    </Flex>
+                </Col>
+            </Row>
         </div>
     );
 };
