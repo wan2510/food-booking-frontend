de706daede61c7423a456e0cdb84ed2239da7c23
Quan <quannahe186968@fpt.edu.vn>
2025-03-04 10:54:11 +0700

    feat: finish login feature

---
+++ src/feature/login/hook/useLogin.js
@@ -0,0 +1,47 @@
+import { useMutation } from '@tanstack/react-query';
+import { LoginApi } from '../../../api/AuthApi';
+import { useNavigate } from 'react-router-dom';
+import { message } from 'antd';
+
+export const useLogin = () => {
+    const navigate = useNavigate();
+
+    const handleLoggedIn = (data) => {
+        const accessToken = data.data.accessToken;
+        const userInfo = data.data.user;
+
+        if (!accessToken || !userInfo) {
+            message.error('Đã xảy ra lỗi, vui lòng thử lại sau');
+            return;
+        }
+
+        localStorage.setItem('access_token', accessToken);
+        localStorage.setItem('user', JSON.stringify(userInfo));
+
+        navigateToPage(userInfo.role);
+    };
+
+    const navigateToPage = (role) => {
+        switch (role) {
+            case 'ROLE_USER':
+                navigate('/home');
+                break;
+            case 'ROLE_ADMIN':
+                navigate('/admin');
+                break;
+            default:
+                navigate('/login');
+                break;
+        }
+    };
+
+    const { mutate: loginMutate } = useMutation({
+        mutationFn: LoginApi,
+        onSuccess: handleLoggedIn,
+        onError: (error) => {
+            message.error(error.response.data.errorMessage);
+        },
+    });
+
+    return { loginMutate };
+};
